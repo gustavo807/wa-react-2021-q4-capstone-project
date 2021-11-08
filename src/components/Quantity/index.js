f4b8b723b57c821c1fe09a0cd7a56a8ea91e1dd7
gustavo <gustavo.garcia@mexmax-internet.com>
2021-11-08 01:20:33 -0600

    Quantity Selector component

---
+++ src/components/Quantity/index.js
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types";
+import { Button, Flex, Input } from "../../styled";
+
+function Quantity(props) {
+  const {
+    quantity,
+    handleIncrement,
+    handleDecrement,
+    isUpDisabled,
+    isDownDisabled,
+  } = props;
+
+  return (
+    <Flex container>
+      <Button disabled={isDownDisabled} onClick={handleDecrement}>
+        -
+      </Button>
+      <Input type="number" value={quantity} disabled />
+      <Button disabled={isUpDisabled} onClick={handleIncrement}>
+        +
+      </Button>
+    </Flex>
+  );
+}
+
+Quantity.propTypes = {
+  quantity: PropTypes.number.isRequired,
+  handleDecrement: PropTypes.func.isRequired,
+  handleIncrement: PropTypes.func.isRequired,
+  isUpDisabled: PropTypes.bool.isRequired,
+  isDownDisabled: PropTypes.bool.isRequired,
+};
+
+Quantity.defaultProps = {
+  quantity: 0,
+  handleDecrement: () => {},
+  handleIncrement: () => {},
+  isUpDisabled: true,
+  isDownDisabled: true,
+};
+
+export default Quantity;
