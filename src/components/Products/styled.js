df5e8b9473841d2592f1dd352daf10d49405e695
gustavo <gustavo.garcia@mexmax-internet.com>
2021-11-08 01:21:52 -0600

    Updated Products component

--- src/components/Products/styled.js
+++ src/components/Products/styled.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { Button } from "../../styled";
 
 export const Cards = styled.div`
   width: 100%;
@@ -8,31 +7,3 @@ export const Cards = styled.div`
   place-content: center;
   gap: 1rem;
 `;
-
-export const Card = styled.div`
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-
-  margin: auto;
-  text-align: center;
-  font-family: arial;
-  height: 100%;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
-
-export const Img = styled.img`
-  width: 100%;
-`;
-
-export const Price = styled.div`
-  color: grey;
-  font-size: 16px;
-`;
-
-export const AddToCart = styled(Button)`
-  width: 100%;
-  margin-top: 0;
-`;
-
