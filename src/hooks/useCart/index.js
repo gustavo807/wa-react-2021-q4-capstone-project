57b8a4cff3df951dd3af583d9c50c86abc660e06
gustavo <gustavo.garcia@mexmax-internet.com>
2021-11-08 01:13:34 -0600

    Shopping Cart Redux state management and useCart custom hook to easily access the state and methods to update it.

---
+++ src/hooks/useCart/index.js
@@ -0,0 +1,35 @@
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import {
+  add,
+  increment,
+  decrement,
+  remove,
+  reset,
+} from "../../redux/slices/cartSlice";
+import {
+  selectProducts,
+  selectCount,
+  selectTotal,
+} from "../../redux/slices/cartSlice";
+
+function useCart() {
+  const products = useSelector(selectProducts);
+  const count = useSelector(selectCount);
+  const total = useSelector(selectTotal);
+  const dispatch = useDispatch();
+
+  return {
+    products,
+    count,
+    total,
+    dispatch,
+    add,
+    increment,
+    decrement,
+    remove,
+    reset,
+  };
+}
+
+export default useCart;
